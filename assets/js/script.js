4d66954a751e13fa0080a013dc84e9e9feb682a9
Klara Winther <klarawinther99@gmail.com>
2026-02-20 15:03:48 +0000

    Contact page

--- assets/js/script.js
+++
@@ -1,7 +0,0 @@
-const button = document.getElementById("revealBtn");
-const text = document.getElementById("hiddenText");
-
-button.addEventListener("click", () => {
-  text.classList.remove("hidden");
-  text.classList.add("rainbow");
-});
